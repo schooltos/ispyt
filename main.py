c14b357f5abd6a2f008ab5dc350b2a8e056a00d0
schooltos <kopchaliyk@gmail.com>
2022-12-09 11:00:22 +0200

    Security fix

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-with open("file.txt", "r") as fl:
-    file_txt = fl.readlines()
-
-def word_count():
+def word_count(text):
     wrd_cnt = 0
     line_count = 0
-    for line in file_txt:
+    for line in text:
         line_count += 1
         f = line.split(" ")
         wrd_cnt = wrd_cnt + len(f)
         print(f"There is {wrd_cnt} words in {line_count} paragraph")
 
-def letter_count():
+def letter_count(text):
     lttr_cnt = 0
     line_count = 0
-    for line in file_txt:
+    for line in text:
         line_count += 1
         lttr_cnt = len(line)
         print(f"There is {lttr_cnt} letters in {line_count} paragraph")
 
-word_count()
-letter_count()
+with open("file.txt", "r") as fl:
+    file_txt = fl.readlines()
+
+word_count(file_txt)
+letter_count(file_txt)
