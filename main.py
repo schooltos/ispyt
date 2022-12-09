0f7a449310e864bb59da472358a8be0580de5e57
schooltos <kopchaliyk@gmail.com>
2022-12-09 09:39:09 +0200

    word_count() func

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-with open("file.txt", "r") as f:
-    file_txt = f.read()
+with open("file.txt", "r") as fl:
+    file_txt = fl.readlines()
 
-print(file_txt)
 
-wrd_cnt = 0
-lttr_cnt = 0
+def word_count():
+    wrd_cnt = 0
+    line_count = 0
+    for line in file_txt:
+        line_count += 1
+        f = line.split(" ")
+        wrd_cnt = wrd_cnt + len(f)
+        print(f"There is {wrd_cnt} words in {line_count} paragraph")
+        
+word_count()
