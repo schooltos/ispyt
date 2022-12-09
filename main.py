041b175e363d67880168aa33b5de6c2c852a4547
schooltos <kopchaliyk@gmail.com>
2022-12-09 10:02:16 +0200

    letter_count() func

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 with open("file.txt", "r") as fl:
     file_txt = fl.readlines()
 
-
 def word_count():
     wrd_cnt = 0
     line_count = 0
@@ -10,5 +9,14 @@ def word_count():
         f = line.split(" ")
         wrd_cnt = wrd_cnt + len(f)
         print(f"There is {wrd_cnt} words in {line_count} paragraph")
-        
+
+def letter_count():
+    lttr_cnt = 0
+    line_count = 0
+    for line in file_txt:
+        line_count += 1
+        lttr_cnt = len(line)
+        print(f"There is {lttr_cnt} letters in {line_count} paragraph")
+
 word_count()
+letter_count()
